c425957e97cbad0a99fc78e7746f58f7b4b4b65c
Konstantin Chugalinskiy <kchugalinskiy@yandex.ru>
2015-10-05 15:45:58 +0300

    Using recursive mutexes on linux

--- ProfilerCore/Linux/ConcurrencyLinux.h
+++ ProfilerCore/Linux/ConcurrencyLinux.h
@@ -6,14 +6,25 @@ namespace Profiler
 	class CriticalSection
 	{
 		pthread_mutex_t mutex;
-		CriticalSection( const CriticalSection & ) {}
-		CriticalSection& operator=( const CriticalSection&) {}
+		pthread_mutexattr_t mutexAttr;
+		CriticalSection( const CriticalSection & );
+		CriticalSection& operator=( const CriticalSection&);
 		
 		void Enter() { pthread_mutex_lock( &mutex ); }
 		void Leave() { pthread_mutex_unlock( &mutex ); }
 	public:
-		CriticalSection() { }
-		~CriticalSection() { }
+		CriticalSection()
+		{
+			pthread_mutexattr_init( &mutexAttr );
+			pthread_mutexattr_settype( &mutexAttr, PTHREAD_MUTEX_RECURSIVE );
+			pthread_mutex_init( &mutex, &mutexAttr );
+		}
+
+		~CriticalSection()
+		{
+			pthread_mutex_destroy( &mutex );
+			pthread_mutexattr_destroy( &mutexAttr );
+		}
 		friend class CriticalSectionScope;
 	};
 }
