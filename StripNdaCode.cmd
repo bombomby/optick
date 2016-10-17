20142aeb36ba3f3af93a649b50124902f9f35e32
s.makeev_local <sergeymakeev@inbox.ru>
2016-10-17 14:38:41 +0300

    Updated build scripts

--- StripNdaCode.cmd
+++ StripNdaCode.cmd
@@ -10,8 +10,6 @@ sunifdef.exe --replace --recurse --filter cpp,h,inl --discard drop --undef MT_PL
 sunifdef.exe --replace --recurse --filter cpp,h,inl --discard drop --undef MT_PLATFORM_DURANGO --undef MT_PLATFORM_ORBIS --undef _XBOX_ONE --undef __ORBIS__ ./BrofilerTest
 sunifdef.exe --replace --recurse --filter cpp,h,inl --discard drop --undef MT_PLATFORM_DURANGO --undef MT_PLATFORM_ORBIS --undef _XBOX_ONE --undef __ORBIS__ ./BrofilerWindowsTest
 
-RemoveXML.exe ./Brofiler/Brofiler_vs2010.csproj
-RemoveXML.exe ./Brofiler/Brofiler_vs2012.csproj
-RemoveXML.exe ./Brofiler/Brofiler_vs2015.csproj
+RemoveXML.exe ./Brofiler/Brofiler.csproj
 
 del Publish.cmd
