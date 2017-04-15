3a766f3f6189f5b680528c6636a4ea5f97bf1f49
Nick Galko <nikolay.galko@gmail.com>
2017-04-15 13:54:03 +0400

    I disabled some fragments, are not used

--- BrofilerCore/Interop.cpp
+++ BrofilerCore/Interop.cpp
@@ -1,29 +1,29 @@
-#include <xmmintrin.h>
-
+//#include <xmmintrin.h>
+//
 namespace MT
 {
-	struct Memory
-	{
-		static void* Alloc(size_t size, size_t align);
-		static void Free(void* p);
-	};
-
+	//	struct Memory
+	//	{
+	//		static void* Alloc(size_t size, size_t align);
+	//		static void Free(void* p);
+	//	};
+	//
 	struct Diagnostic
 	{
 		static void ReportAssert(const char* condition, const char* description, const char* sourceFile, int sourceLine);
 	};
-
-
-	void* Memory::Alloc(size_t size, size_t align)
-	{
-		return _mm_malloc(size, align);
-	}
-
-	void Memory::Free(void* p)
-	{
-		_mm_free(p);
-	}
-
+	//
+	//
+	//	void* Memory::Alloc(size_t size, size_t align)
+	//	{
+	//		return _mm_malloc(size, align);
+	//	}
+	//
+	//	void Memory::Free(void* p)
+	//	{
+	//		_mm_free(p);
+	//	}
+	//
 	void Diagnostic::ReportAssert(const char*, const char*, const char*, int)
 	{
 		__debugbreak();
