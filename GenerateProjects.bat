7b70c0a93ef7cd14ec6a13a8d6f17afe9f838d59
bombom_by <nb_grif@mail.ru>
2018-10-03 00:31:14 +0100

    Refactoring genie

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,7 +1,7 @@
-Tools\genie.exe vs2010 genie.lua
-Tools\genie.exe vs2012 genie.lua
-Tools\genie.exe vs2015 genie.lua
-Tools\genie.exe vs2017 genie.lua
+Tools\genie.exe vs2010 --file=genie.lua
+Tools\genie.exe vs2012 --file=genie.lua
+Tools\genie.exe vs2015 --file=genie.lua
+Tools\genie.exe vs2017 --file=genie.lua
 
 rem COPY /Y BrofilerDurangoTest\BrofilerDurangoTest.vcxproj BrofilerDurangoTest\BrofilerDurangoTest_backup.vcxproj
 rem COPY /Y BrofilerDurangoTest\BrofilerDurangoTest.vcxproj.filters BrofilerDurangoTest\BrofilerDurangoTest_backup.vcxproj.filters
