bac0c31820d15ca8032aed41ac614c9f528fa46d
bombom_by <nb_grif@mail.ru>
2019-01-16 00:31:19 +0000

    Adding screenshot to the Vulkan sample

--- Samples/WindowsVulkan/multithreading.cpp
+++ Samples/WindowsVulkan/multithreading.cpp
@@ -13,6 +13,7 @@
 #include <vector>
 #include <thread>
 #include <random>
+#include <fstream>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -28,6 +29,37 @@
 #include "VulkanModel.hpp"
 
 #define ENABLE_VALIDATION false
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+std::wstring g_ScreenshotRequest;
+bool g_TakingScreenshot = false;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool OnBrofilerStateChanged(Brofiler::BroState state)
+{
+	if (state == Brofiler::BRO_STOP_CAPTURE)
+	{
+		wchar_t tempPath[MAX_PATH] = { 0 };
+		GetTempPath(MAX_PATH, tempPath);
+		std::wstring fullPath(tempPath);
+		g_ScreenshotRequest = fullPath + L"BrofilerScreenshot.bmp";
+		g_TakingScreenshot = true;
+	}
+
+	if (state == Brofiler::BRO_DUMP_CAPTURE)
+	{
+		if (g_TakingScreenshot)
+			return false;
+
+		// Attach screenshot
+		Brofiler::AttachFile(Brofiler::BroFile::BRO_IMAGE, "Screenshot.bmp", g_ScreenshotRequest.c_str());
+
+		// Remove temp file
+		_wremove(g_ScreenshotRequest.c_str());
+		g_ScreenshotRequest.clear();
+	}
+	return true;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class VulkanExample : public VulkanExampleBase
 {
@@ -139,6 +171,8 @@ public:
 		threadPool.setThreadCount(numThreads);
 		numObjectsPerThread = 512 / numThreads;
 		rndEngine.seed(benchmark.active ? 0 : (unsigned)time(nullptr));
+		
+		BROFILER_SET_STATE_CHANGED_CALLBACK(OnBrofilerStateChanged);
 	}
 
 	~VulkanExample()
@@ -597,6 +631,11 @@ public:
 
 		VK_CHECK_RESULT(vkQueueSubmit(queue, 1, &submitInfo, renderFence));
 
+		if (g_TakingScreenshot) {
+			saveScreenshot(g_ScreenshotRequest.c_str());
+			g_TakingScreenshot = false;			
+		}
+
 		VulkanExampleBase::submitFrame();
 	}
 
@@ -635,6 +674,254 @@ public:
 			overlay->checkBox("Skybox", &displaySkybox);
 		}
 
+	}
+
+	// szPathName : Specifies the pathname        -> the file path to save the image
+	// lpBits    : Specifies the bitmap bits      -> the buffer (content of the) image
+	// w    : Specifies the image width
+	// h    : Specifies the image height
+	bool saveBitmap(std::ofstream& stream, void* lpBits, int w, int h) {
+		BITMAPINFOHEADER BMIH;                         // BMP header
+		BMIH.biSize = sizeof(BITMAPINFOHEADER);
+		BMIH.biSizeImage = w * h * 3;
+		// Create the bitmap for this OpenGL context
+		BMIH.biSize = sizeof(BITMAPINFOHEADER);
+		BMIH.biWidth = w;
+		BMIH.biHeight = h;
+		BMIH.biPlanes = 1;
+		BMIH.biBitCount = 24;
+		BMIH.biCompression = BI_RGB;
+		BMIH.biSizeImage = w * h * 3;
+
+		BITMAPFILEHEADER bmfh;                         // Other BMP header
+		int nBitsOffset = sizeof(BITMAPFILEHEADER) + BMIH.biSize;
+		LONG lImageSize = BMIH.biSizeImage;
+		LONG lFileSize = nBitsOffset + lImageSize;
+		bmfh.bfType = 'B' + ('M' << 8);
+		bmfh.bfOffBits = nBitsOffset;
+		bmfh.bfSize = lFileSize;
+		bmfh.bfReserved1 = bmfh.bfReserved2 = 0;
+
+		// Write the bitmap file header               // Saving the first header to file
+		stream.write((const char*)&bmfh, sizeof(BITMAPFILEHEADER));
+
+		// And then the bitmap info header            // Saving the second header to file
+		stream.write((const char*)&BMIH, sizeof(BITMAPINFOHEADER));
+
+		// Finally, write the image data itself
+		//-- the data represents our drawing          // Saving the file content in lpBits to file
+		stream.write((const char*)lpBits, lImageSize);
+
+		stream.flush();
+
+		return true;
+	}
+
+	// Take a screenshot from the current swapchain image
+	// This is done using a blit from the swapchain image to a linear image whose memory content is then saved as a ppm image
+	// Getting the image date directly from a swapchain image wouldn't work as they're usually stored in an implementation dependant optimal tiling format
+	// Note: This requires the swapchain images to be created with the VK_IMAGE_USAGE_TRANSFER_SRC_BIT flag (see VulkanSwapChain::create)
+	void saveScreenshot(const wchar_t* filename)
+	{
+		bool supportsBlit = true;
+
+		// Check blit support for source and destination
+		VkFormatProperties formatProps;
+
+		// Check if the device supports blitting from optimal images (the swapchain images are in optimal format)
+		vkGetPhysicalDeviceFormatProperties(physicalDevice, swapChain.colorFormat, &formatProps);
+		if (!(formatProps.optimalTilingFeatures & VK_FORMAT_FEATURE_BLIT_SRC_BIT)) {
+			std::cerr << "Device does not support blitting from optimal tiled images, using copy instead of blit!" << std::endl;
+			supportsBlit = false;
+		}
+
+		// Check if the device supports blitting to linear images 
+		vkGetPhysicalDeviceFormatProperties(physicalDevice, VK_FORMAT_R8G8B8A8_UNORM, &formatProps);
+		if (!(formatProps.linearTilingFeatures & VK_FORMAT_FEATURE_BLIT_DST_BIT)) {
+			std::cerr << "Device does not support blitting to linear tiled images, using copy instead of blit!" << std::endl;
+			supportsBlit = false;
+		}
+
+		// Source for the copy is the last rendered swapchain image
+		VkImage srcImage = swapChain.images[currentBuffer];
+
+		// Create the linear tiled destination image to copy to and to read the memory from
+		VkImageCreateInfo imageCreateCI(vks::initializers::imageCreateInfo());
+		imageCreateCI.imageType = VK_IMAGE_TYPE_2D;
+		// Note that vkCmdBlitImage (if supported) will also do format conversions if the swapchain color format would differ
+		imageCreateCI.format = VK_FORMAT_R8G8B8A8_UNORM;
+		imageCreateCI.extent.width = width;
+		imageCreateCI.extent.height = height;
+		imageCreateCI.extent.depth = 1;
+		imageCreateCI.arrayLayers = 1;
+		imageCreateCI.mipLevels = 1;
+		imageCreateCI.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		imageCreateCI.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageCreateCI.tiling = VK_IMAGE_TILING_LINEAR;
+		imageCreateCI.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+		// Create the image
+		VkImage dstImage;
+		VK_CHECK_RESULT(vkCreateImage(device, &imageCreateCI, nullptr, &dstImage));
+		// Create memory to back up the image
+		VkMemoryRequirements memRequirements;
+		VkMemoryAllocateInfo memAllocInfo(vks::initializers::memoryAllocateInfo());
+		VkDeviceMemory dstImageMemory;
+		vkGetImageMemoryRequirements(device, dstImage, &memRequirements);
+		memAllocInfo.allocationSize = memRequirements.size;
+		// Memory must be host visible to copy from
+		memAllocInfo.memoryTypeIndex = vulkanDevice->getMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+		VK_CHECK_RESULT(vkAllocateMemory(device, &memAllocInfo, nullptr, &dstImageMemory));
+		VK_CHECK_RESULT(vkBindImageMemory(device, dstImage, dstImageMemory, 0));
+
+		// Do the actual blit from the swapchain image to our host visible destination image
+		VkCommandBuffer copyCmd = vulkanDevice->createCommandBuffer(VK_COMMAND_BUFFER_LEVEL_PRIMARY, true);
+
+		// Transition destination image to transfer destination layout
+		vks::tools::insertImageMemoryBarrier(
+			copyCmd,
+			dstImage,
+			0,
+			VK_ACCESS_TRANSFER_WRITE_BIT,
+			VK_IMAGE_LAYOUT_UNDEFINED,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{ VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 });
+
+		// Transition swapchain image from present to transfer source layout
+		vks::tools::insertImageMemoryBarrier(
+			copyCmd,
+			srcImage,
+			VK_ACCESS_MEMORY_READ_BIT,
+			VK_ACCESS_TRANSFER_READ_BIT,
+			VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+			VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{ VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 });
+
+		// If source and destination support blit we'll blit as this also does automatic format conversion (e.g. from BGR to RGB)
+		if (supportsBlit)
+		{
+			// Define the region to blit (we will blit the whole swapchain image)
+			VkOffset3D blitSize;
+			blitSize.x = width;
+			blitSize.y = height;
+			blitSize.z = 1;
+			VkImageBlit imageBlitRegion{};
+			imageBlitRegion.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlitRegion.srcSubresource.layerCount = 1;
+			imageBlitRegion.srcOffsets[1] = blitSize;
+			imageBlitRegion.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlitRegion.dstSubresource.layerCount = 1;
+			imageBlitRegion.dstOffsets[1] = blitSize;
+
+			// Issue the blit command
+			vkCmdBlitImage(
+				copyCmd,
+				srcImage, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+				dstImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+				1,
+				&imageBlitRegion,
+				VK_FILTER_NEAREST);
+		}
+		else
+		{
+			// Otherwise use image copy (requires us to manually flip components)
+			VkImageCopy imageCopyRegion{};
+			imageCopyRegion.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageCopyRegion.srcSubresource.layerCount = 1;
+			imageCopyRegion.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageCopyRegion.dstSubresource.layerCount = 1;
+			imageCopyRegion.extent.width = width;
+			imageCopyRegion.extent.height = height;
+			imageCopyRegion.extent.depth = 1;
+
+			// Issue the copy command
+			vkCmdCopyImage(
+				copyCmd,
+				srcImage, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+				dstImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+				1,
+				&imageCopyRegion);
+		}
+
+		// Transition destination image to general layout, which is the required layout for mapping the image memory later on
+		vks::tools::insertImageMemoryBarrier(
+			copyCmd,
+			dstImage,
+			VK_ACCESS_TRANSFER_WRITE_BIT,
+			VK_ACCESS_MEMORY_READ_BIT,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+			VK_IMAGE_LAYOUT_GENERAL,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{ VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 });
+
+		// Transition back the swap chain image after the blit is done
+		vks::tools::insertImageMemoryBarrier(
+			copyCmd,
+			srcImage,
+			VK_ACCESS_TRANSFER_READ_BIT,
+			VK_ACCESS_MEMORY_READ_BIT,
+			VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+			VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{ VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 });
+
+		vulkanDevice->flushCommandBuffer(copyCmd, queue);
+
+		// Get layout of the image (including row pitch)
+		VkImageSubresource subResource{ VK_IMAGE_ASPECT_COLOR_BIT, 0, 0 };
+		VkSubresourceLayout subResourceLayout;
+		vkGetImageSubresourceLayout(device, dstImage, &subResource, &subResourceLayout);
+
+		// Map image memory so we can start copying from it
+		const char* data;
+		vkMapMemory(device, dstImageMemory, 0, VK_WHOLE_SIZE, 0, (void**)&data);
+		data += subResourceLayout.offset;
+
+		// If source is BGR (destination is always RGB) and we can't use blit (which does automatic conversion), we'll have to manually swizzle color components
+		bool isBGR = false;
+		// Check if source is BGR 
+		// Note: Not complete, only contains most common and basic BGR surface formats for demonstation purposes
+		if (!supportsBlit)
+		{
+			std::vector<VkFormat> formatsBGR = { VK_FORMAT_B8G8R8A8_SRGB, VK_FORMAT_B8G8R8A8_UNORM, VK_FORMAT_B8G8R8A8_SNORM };
+			isBGR = (std::find(formatsBGR.begin(), formatsBGR.end(), swapChain.colorFormat) != formatsBGR.end());
+		}
+
+
+		std::vector<char> buffer;
+		buffer.resize(width * height * 3);
+		
+		const char* pImg = data;
+		for (uint32_t y = 0; y < height; y++)
+		{
+			const char* row = (const char*)pImg;
+			for (uint32_t x = 0; x < width; x++)
+			{
+				char* out = &buffer[((height - 1 - y) * width + x) * 3];
+
+				out[0] = row[isBGR ? 0 : 2];
+				out[1] = row[1];
+				out[2] = row[isBGR ? 2 : 0];
+				
+				row += 4;
+			}
+			pImg += subResourceLayout.rowPitch;
+		}
+
+		std::ofstream file(filename, std::ios::out | std::ios::binary);
+		saveBitmap(file, buffer.data(), width, height);
+
+		std::cout << "Screenshot saved to disk" << std::endl;
+
+		// Clean up resources
+		vkUnmapMemory(device, dstImageMemory);
+		vkFreeMemory(device, dstImageMemory, nullptr);
+		vkDestroyImage(device, dstImage, nullptr);
 	}
 };
 
