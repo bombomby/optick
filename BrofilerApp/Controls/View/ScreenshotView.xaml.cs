a15ca713ed5843dd2e90a3c418c6208ccfad7e26
Samsonov SA <samsonovsa@yandex.ru>
2019-02-03 20:50:18 +0300

    Implement magnifyingGlass

---
+++ BrofilerApp/Controls/View/ScreenshotView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Profiler.Controls.View
+{
+    /// <summary>
+    /// Interaction logic for ScreenshotView.xaml
+    /// </summary>
+    public partial class ScreenshotView : MetroWindow
+    {
+        public ScreenshotView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BrofilerApp/Controls/ViewModel/ScreenshotViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Profiler.Data;
+
+namespace Profiler.Controls.ViewModel
+{
+    public class ScreenshotViewModel: BaseViewModel
+    {
+        ImageSource _attachmentImage;
+        public ImageSource AttachmentImage
+        {
+            get { return _attachmentImage; }
+            set { SetField(ref _attachmentImage, value); }
+        }
+
+        public string Title { get; set; }
+
+        public ScreenshotViewModel(SummaryPack.Attachment attachment, string nameCapture)
+        {
+            if(attachment.FileType == SummaryPack.Attachment.Type.BRO_IMAGE)
+            {
+                BitmapImage imageSource = new BitmapImage();
+                imageSource.BeginInit();
+                imageSource.StreamSource = attachment.Data;
+                imageSource.EndInit();
+
+                AttachmentImage = imageSource;
+            }
+            Title = String.Format("{0} ({1})", attachment.Name, nameCapture);
+        }
+
+    }
+}
