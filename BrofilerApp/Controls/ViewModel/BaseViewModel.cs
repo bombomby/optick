35e0f5ef61b88a3e3eeb702dab2a0ba6264cc951
Vadim Slyusarev <nb_grif@mail.ru>
2019-02-17 09:30:16 +0000

    Reverting the previous revert for the upcoming merge (#60)

---
+++ BrofilerApp/Controls/ViewModel/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;                      //INotifyPropertyChanged
+using System.Runtime.CompilerServices;            //CallerMemberName
+// using System.Windows;                             //DependencyObject, DependencyProperty            
+
+namespace Profiler.Controls.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged //DependencyObject, 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value,[CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+    }
+}
