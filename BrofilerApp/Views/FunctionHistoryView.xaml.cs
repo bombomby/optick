5dcab99a31d272c50aae5caeb7a10523a7bb2cfc
Vadim Slyusarev <nb_grif@mail.ru>
2019-02-24 20:07:30 +0000

    Adding Hamburger menu

---
+++ BrofilerApp/Views/FunctionHistoryView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Profiler.Views
+{
+    /// <summary>
+    /// Interaction logic for FunctionHistoryView.xaml
+    /// </summary>
+    public partial class FunctionHistoryView : UserControl
+    {
+        public FunctionHistoryView()
+        {
+            InitializeComponent();
+
+            FrameChart.TooltipTimeout = new TimeSpan(0, 0, 0, 0, 100);
+            FrameChart.DataTooltip.Background = FindResource("BroBackground") as SolidColorBrush;
+            FrameChart.DataTooltip.BorderBrush = FindResource("AccentColorBrush") as SolidColorBrush;
+            FrameChart.DataTooltip.BorderThickness = new Thickness(0.5);
+        }
+    }
+}
