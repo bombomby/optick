5665e022be371ae3c364584b2ab7fb778bcc6f41
bombom_by <nb_grif@mail.ru>
2019-08-18 23:44:00 +0100

    [v1.2.6] Adding support for error reporting through Sentry lib.

--- gui/App.xaml.cs
+++ gui/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Reflection;
 using System.IO;
 using System.Diagnostics;
+using Sentry;
 
 namespace Profiler
 {
@@ -14,19 +15,42 @@ namespace Profiler
 
         static App()
         {
-			//AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(OnAssemblyResolve);
-        }
 
-		//static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
-		//{
-		//	return AutoEmbedLibs.EmbeddedAssembly.Get(args.Name);
-		//}
+		}
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-        }
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+			base.OnStartup(e);
+		}
 
+		private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			Exception ex = e.ExceptionObject as Exception;
+			ReportError(ex);
+		}
 
-    }
+		private void Optick_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			e.Handled = ReportError(e.Exception);
+		}
+
+		bool ReportError(Exception ex)
+		{
+			Exception rootException = ex;
+
+			while (rootException.InnerException != null)
+				rootException = rootException.InnerException;
+
+			if (MessageBox.Show("Unhandled Exception:\n" + rootException.ToString(), "Optick Crashed! Send report?", MessageBoxButton.OKCancel, MessageBoxImage.Error) == MessageBoxResult.OK)
+			{
+				using (SentrySdk.Init("https://52c8ab53c0cf47f28263fc211ebd4d38@sentry.io/1493349"))
+				{
+					SentrySdk.CaptureException(rootException);
+				}
+				return true;
+			}
+			return false;
+		}
+	}
 }
