16b13cc21439c8981a4f89595a9244cef242bc9f
Honeybunch <amt3824@g.rit.edu>
2021-05-31 14:15:04 -0700

    Seems like this codebase should be using tabs so I will not argue

--- samples/ConsoleAppCAPI/main.cpp
+++ samples/ConsoleAppCAPI/main.cpp
@@ -16,20 +16,20 @@
 // Testing OPTICK_APP macro for startup performance analysis
 void TestOptickApp(Test::Engine *engine)
 {
-    OptickAPI_StartCapture();
-    for (int i = 0; i < 3; ++i)
-        engine->Update();
-    OptickAPI_StopCapture(NULL, 0);
+	OptickAPI_StartCapture();
+	for (int i = 0; i < 3; ++i)
+		engine->Update();
+	OptickAPI_StopCapture(NULL, 0);
 }
 
 // Testing OPTICK_START_CAPTURE/OPTICK_STOP_CAPTURE/OPTICK_SAVE_CAPTURE for performance automation
 void TestAutomation(Test::Engine *engine)
 {
-    OptickAPI_StartCapture();
-    for (int i = 0; i < 3; ++i)
-        engine->Update();
-    const char outFileName[] = "ConsoleApp";
-    OptickAPI_StopCapture(outFileName, sizeof(outFileName));
+	OptickAPI_StartCapture();
+	for (int i = 0; i < 3; ++i)
+		engine->Update();
+	const char outFileName[] = "ConsoleApp";
+	OptickAPI_StopCapture(outFileName, sizeof(outFileName));
 }
 
 #if OPTICK_MSVC
@@ -38,43 +38,43 @@ int wmain()
 int main()
 #endif
 {
-    printf("Starting profiler test.\n");
+	printf("Starting profiler test.\n");
 
-    Test::Engine engine;
-    printf("Engine successfully created.\n");
+	Test::Engine engine;
+	printf("Engine successfully created.\n");
 
-    printf("Starting main loop update.\n");
+	printf("Starting main loop update.\n");
 
-    // Setting memory allocators
-    OptickAPI_SetAllocator(
-        [](size_t size) -> void * { return malloc(size); },
-        [](void *p)
-        { free(p); },
-        []() { /* Do some TLS initialization here if needed */ });
+	// Setting memory allocators
+	OptickAPI_SetAllocator(
+		[](size_t size) -> void * { return malloc(size); },
+		[](void *p)
+		{ free(p); },
+		[]() { /* Do some TLS initialization here if needed */ });
 
-    //TestOptickApp(&engine);
-    //TestAutomation(&engine);
+	//TestOptickApp(&engine);
+	//TestAutomation(&engine);
 
-    // Unlike C++ API, you must call StartCapture explicitly
-    // Otherwise while capture files may be generated correctly the
-    // OptickApp will fail to generate a preview when live captures are stopped
-    OptickAPI_StartCapture();
+	// Unlike C++ API, you must call StartCapture explicitly
+	// Otherwise while capture files may be generated correctly the
+	// OptickApp will fail to generate a preview when live captures are stopped
+	OptickAPI_StartCapture();
 
-    bool needExit = false;
-    while (!needExit)
-    {
-        OptickAPI_NextFrame();
+	bool needExit = false;
+	while (!needExit)
+	{
+		OptickAPI_NextFrame();
 
-        if (!engine.Update())
-            break;
+		if (!engine.Update())
+			break;
 
-        printf(".");
-        fflush(stdout);
-    }
+		printf(".");
+		fflush(stdout);
+	}
 
-    OptickAPI_Shutdown();
+	OptickAPI_Shutdown();
 
-    return 0;
+	return 0;
 }
 
 #if OPTICK_MSVC
