19da254e0e741334bfda79ff989cf8adedcf7c62
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2021-09-18 00:37:14 +0200

    Add stats per function call to Function Chart (GUI)

--- gui/Profiler.Data/FunctionStats.cs
+++ gui/Profiler.Data/FunctionStats.cs
@@ -47,21 +47,105 @@ namespace Profiler.Data
 		public FrameGroup Group { get; set; }
 		public EventDescription Description { get; set; }
 
+        // Time spent in this function during an average frame
         public double AvgTotal
         {
             get { return Samples.Count > 0 ? Samples.Average(s => s.Total) : 0.0; }
         }
 
+        // Time spent working in this function during an average frame
         public double AvgWork
         {
             get { return Samples.Count > 0 ? Samples.Average(s => s.Work) : 0.0; }
         }
 
+        // Time spent waiting in this function during an average frame
         public double AvgWait
         {
             get { return Samples.Count > 0 ? Samples.Average(s => s.Wait) : 0.0; }
         }
 
+        // Fastest time
+        public double MinPerCall
+        {
+            get
+            {
+                double res = Double.MaxValue;
+                foreach (Sample s in Samples)
+                {
+                    foreach (Entry e in s.Entries)
+                    {
+                        if (res > e.Duration)
+                        {
+                            res = e.Duration;
+                        }
+                    }
+                }
+
+                return res;
+            }
+        }
+
+        // Slowest time
+        public double MaxPerCall
+        {
+            get
+            {
+                double res = 0.0;
+                foreach (Sample s in Samples)
+                {
+                    foreach (Entry e in s.Entries)
+                    {
+                        if (res < e.Duration)
+                        {
+                            res = e.Duration;
+                        }
+                    }
+                }
+
+                return res;
+            }
+        }
+
+        // Average function time (averaged over calls, not frames)
+        public double AvgTotalPerCall
+        {
+            get
+            {
+                int numCalls = 0;
+                double sum = 0.0;
+                foreach (Sample s in Samples)
+                {
+                    numCalls += s.Count;
+                    sum += s.Total;
+                }
+
+                return sum / numCalls;
+            }
+        }
+
+        // Standard deviation of the individual function times
+        public double StdDevPerCall
+        {
+            get
+            {
+                double avg = this.AvgTotalPerCall;
+
+                double sum = 0.0;
+                int num = 0;
+                foreach (Sample s in Samples)
+                {
+                    num += s.Count;
+                    foreach (Entry e in s.Entries)
+                    {
+                        double x = e.Duration - avg;
+                        sum += x * x;
+                    }
+                }
+
+                return num > 0 ? Math.Sqrt(sum / num) : 0.0;
+            }
+        }
 
         public FunctionStats(FrameGroup group, EventDescription desc)
 		{
