6f4d40e25767598a19f581d510fe79391183f3b8
Anton Yudintsev <anton@gaijin.team>
2022-03-31 20:44:55 +0300

    undefine VKAPI_PTR define, if it was defined in OPTICK
    otherwise, depending on order of includes we get macro redefinition warning

--- src/optick.h
+++ src/optick.h
@@ -114,6 +114,7 @@ struct VkSubmitInfo;
 struct VkCommandBufferBeginInfo;
 
 #ifndef VKAPI_PTR
+#define OPTICK_VKAPI_PTR_DEFINED 1
 #if defined(_WIN32)
     // On Windows, Vulkan commands use the stdcall convention
 	#define VKAPI_PTR  __stdcall
@@ -140,6 +141,10 @@ typedef void (VKAPI_PTR *PFN_vkDestroyCommandPool_)(VkDevice device, VkCommandPo
 typedef void (VKAPI_PTR *PFN_vkDestroyQueryPool_)(VkDevice device, VkQueryPool queryPool, const VkAllocationCallbacks* pAllocator);
 typedef void (VKAPI_PTR *PFN_vkDestroyFence_)(VkDevice device, VkFence fence, const VkAllocationCallbacks* pAllocator);
 typedef void (VKAPI_PTR *PFN_vkFreeCommandBuffers_)(VkDevice device, VkCommandPool commandPool, uint32_t commandBufferCount, const VkCommandBuffer* pCommandBuffers);
+
+#if OPTICK_VKAPI_PTR_DEFINED
+#undef VKAPI_PTR
+#endif
 
 // D3D12 Forward Declarations
 struct ID3D12CommandList;
